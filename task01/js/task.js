8c45dacd2151136c661928b1f9ac4f2189402d1d
shimaa191 <64148155+shimaa191@users.noreply.github.com>
2023-05-24 23:13:48 +0300

    Add files via upload

--- task01/js/task.js
+++ task01/js/task.js
@@ -1,28 +1,51 @@
-function multiplyFive(num) {
-  let res = 1;
-  while (num > 1) {
-    res *= num;
-    num--;
-  }
-  return res;
+function dividByFive(num) {
+  if (num % 5 == 0) return "number is divided by 5";
+  else return "number isn't divided by 5";
 }
 
 function hello() {
-  console.log("Hello World");
+  return "Hello World";
 }
 
 function isLeapYear(year) {
-  if (year % 4 == 0 && year != 100) return "it's a Leap year";
-  else if (year % 400 == 0) return "it's a Leap year";
+  if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+    return "it's a Leap year";
   else return "it isn't a Leap year";
 }
 
-// console.log(multiplyFive(7));
+// // console.log(multiplyFive(7));
+// // hello();
+
+// let year = +prompt("enter the year"),
+//   num = +prompt("enter the number");
+
 // hello();
+// console.log(isLeapYear(year));
+// console.log(multiplyFive(num));
 
-let year = +prompt("enter the year"),
-  num = +prompt("enter the number");
+const btnPrint = document.querySelector("#print"),
+  btnyear = document.querySelector("#year"),
+  btndivid = document.querySelector("#divid"),
+  btnrandomNum = document.querySelector("#randomNum"),
+  show = document.querySelector("#show");
 
-hello();
-console.log(isLeapYear(year));
-console.log(multiplyFive(num));
+btnPrint.addEventListener("click", function () {
+  // let cahch = hello();
+  show.innerText = hello();
+});
+
+btnyear.addEventListener("click", function () {
+  let year = +prompt("enter the year");
+  show.innerText = isLeapYear(year);
+});
+
+btndivid.addEventListener("click", function () {
+  let num = +prompt("enter the number");
+  show.innerText = dividByFive(num);
+});
+
+btnrandomNum.addEventListener("click", function () {
+  //Math.floor(Math.random() * (max - min) ) + min;
+  let num = Math.floor(Math.random() * (1000 - 100)) + 100;
+  show.innerText = num;
+});
